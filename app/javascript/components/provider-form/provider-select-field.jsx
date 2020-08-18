8421a7b9eedee724d1985556eb22567166694574
Dávid Halász <dhalasz@redhat.com>
2020-08-18 15:02:15 +0200

    bump2

--- app/javascript/components/provider-form/provider-select-field.jsx
+++ app/javascript/components/provider-form/provider-select-field.jsx
@@ -6,6 +6,7 @@ import { useFormApi, useFieldApi } from '@@ddf';
 import { components } from '@data-driven-forms/pf3-component-mapper';
 import { EditingContext, loadProviderFields } from './index';
 
+/*
 const extractInitialValues = ({ name, initialValue, fields }) => {
   const children = fields ? fields.reduce((obj, field) => ({ ...obj, ...extractInitialValues(field) }), {}) : {};
   const item = name && initialValue ? { [name]: initialValue } : undefined;
@@ -26,10 +27,30 @@ const ProviderSelectField = ({ kind, ...props }) => {
       loadProviderFields(kind, value).then((fields) => {
         if (isMounted.current) {
           setState(({ fields: [firstField] }) => ({ fields: [firstField, ...fields] }));
-          const initialValues = extractInitialValues({ fields });
-          formOptions.initialize(Object.keys(initialValues).reduce((obj, key) => set(obj, key, initialValues[key]), { value }));
+          // const initialValues = extractInitialValues({ fields });
+          // formOptions.initialize(Object.keys(initialValues).reduce((obj, key) => set(obj, key, initialValues[key]), { value }));
         }
       }).then(miqSparkleOff);
+    }
+  }, [value]);
+
+  return <components.Select {...props} />;
+};
+*/
+
+
+const ProviderSelectField = ({ kind, ...props }) => {
+  const { input: { value } } = useFieldApi(props);
+  const { isDisabled: edit } = props;
+  const { setState } = useContext(EditingContext);
+
+  useEffect(() => {
+    if (!edit && value) {
+      loadProviderFields(kind, value).then((fields) => {
+        setState(({ fields: [firstField] }) => ({
+          fields: [firstField, ...fields],
+        }));
+      });
     }
   }, [value]);
 
